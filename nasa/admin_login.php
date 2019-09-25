774a142e8cf79a832ef6fd16a18cd2fbbfbff451
Jan Kebrle <s.kebrle@gmail.com>
2019-09-25 13:37:15 +0200

    Login page and login check

---
+++ nasa/admin_login.php
@@ -0,0 +1,23 @@
+<?php
+
+?>
+
+<!DOCTYPE html>
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <title>Admin login</title>
+</head>
+<body>
+<h1> If you are admin please login</h1>
+
+
+<form action="../src/login_check.php" method="post">
+    Login: <input type="text" name="login"><br>
+    Password: <input type="password" name="password"><br>
+    <input type="submit">
+</form>
+
+
+</body>
+</html>
---
+++ src/login_check.php
@@ -0,0 +1,2 @@
+<?php
+echo "triedd to login";
