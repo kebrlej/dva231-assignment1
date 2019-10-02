49bb74574f050f43bb6ac198463e36383539727c
Jan Kebrle <s.kebrle@gmail.com>
2019-10-02 17:17:32 +0200

    Script for loading articles from json files and storing them in database.

--- src/road2database.php
+++ src/road2database.php
@@ -5,15 +5,23 @@ if ($db->connect_errno) {
     die("Could not connect: " . mysqli_connect_error());
 }/* create a prepared statement and bind */
 
+$title = "Big title";
+$imgurl = "some url";
+$content = "Nice content";
 
-$newsfile = get_available_news_files();
-foreach ($newsfile as $file) {
+$statement = $db->prepare("INSERT INTO news_article(title,imgurl,content) VALUES (?,?,?)");
+$statement->bind_param("sss", $title, $imgurl, $content);
+
+foreach (get_available_news_files() as $file) {
     $articles = read_news_file_by_name($file)['news'];
     foreach ($articles as $article) {
-        /*echo $article['title'];
-        echo $article['imgurl'];
-        echo $article['content'];*/
-        
+        $title = $article['title'];
+        $imgurl = $article['imgurl'];
+        $content = $article['content'];
+        $statement->execute();
     }
 }
+
+
+$statement->close();
 $db->close();
