0ce2be7c6b17614da9c4a8422d0bd0e264acf706
Angelic0strich <julien.savornin@gmail.com>
2019-10-02 17:00:06 +0200

    connecting to database

--- src/road2database.php
+++ src/road2database.php
@@ -1,13 +1,19 @@
 <?php
 require "file_reader.php";
+$db = new mysqli("localhost", "root", "", "assignment3");
+if ($db->connect_errno) {
+    die("Could not connect: " . mysqli_connect_error());
+}/* create a prepared statement and bind */
+
+
 $newsfile = get_available_news_files();
-foreach ($newsfile as $file)
-{
+foreach ($newsfile as $file) {
     $articles = read_news_file_by_name($file)['news'];
-    foreach ($articles as $article)
-    {
-        echo $article['title'];
+    foreach ($articles as $article) {
+        /*echo $article['title'];
         echo $article['imgurl'];
-        echo $article['content'];
+        echo $article['content'];*/
+        
     }
 }
+$db->close();
